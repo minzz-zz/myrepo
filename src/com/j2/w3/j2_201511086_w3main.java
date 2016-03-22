5d66d4cae6f818a99ab441b82790733b35c2a11f
minzz-zz <mingg0321@naver.com>
2016-03-22 14:34:38 +0900

    201511086_w3_hw

---
+++ src/com/j2/w3/j2_201511086_w3main.java
@@ -0,0 +1,90 @@
+package com.j2.w3;
+import java.util.Date;
+import com.sd.turtle.WeightTurtle;
+
+public class j2_201511086_w3main {
+    public static void main(String[] args) {
+        String[] B={"John", "Adam", "Skrien", "Smith", "Jones"};
+        Comparator stringComp=new StringComparator();
+        Sorter.sort(B, stringComp);
+
+        Integer[] C = {new Integer(3), new Integer(1), new Integer(4), new Integer(2)};
+        Comparator integerComp=new IntegerComparator();
+        Sorter.sort(C, integerComp);
+
+        for(int i=0; i<B.length; i++)
+            System.out.println("B["+i+"]="+B[i]);
+        for(int i=0; i<C.length; i++)
+            System.out.println("C["+i+"]="+C[i]);
+        
+        WeightTurtle t1 = new WeightTurtle(40);
+        WeightTurtle t2 = new WeightTurtle(30);
+        WeightTurtle t3 = new WeightTurtle(50);
+        WeightTurtle[] wt={t1, t2, t3};
+        Comparator turtleComp=new TurtleComparator();
+        Sorter.sort(wt, turtleComp);
+        
+        for (int i=0;i<wt.length;i++)
+          System.out.println("wt["+i+"]="+wt[i].getWeight());
+    }
+}
+
+class Sorter {
+  public Sorter() {}
+    public static void sort(Object[] data, Comparator comp) {
+        for(int i=data.length-1; i>=1; i--) {
+            int indexOfMax=0;
+            for(int j=1; j<=i; j++) {
+                if(comp.compare(data[j], data[indexOfMax]) > 0)
+                    indexOfMax=j;
+                }
+                Object temp=data[i];
+                data[i]=data[indexOfMax];
+                data[indexOfMax]=temp;
+        }
+    }
+}
+
+interface Comparator {
+    public int compare(Object o1, Object o2);
+    public boolean equals(Object o);
+}
+
+class IntegerComparator implements Comparator {
+    public IntegerComparator() {}
+    public int compare(Object o1, Object o2) {
+        return (Integer)o1 - (Integer)o2;
+    }
+}
+
+class StringComparator implements Comparator {
+    public StringComparator() {}
+    public int compare(Object o1, Object o2) {
+        String s1=(String)o1;
+        String s2=(String)o2;
+        return s1.compareTo(s2);
+        //return Integer.parseInt((String) o1) -
+        //   Integer.parseInt((String) o2);
+    }
+}
+
+class DateComparator implements Comparator {
+    public DateComparator() {}
+    public int compare(Object o1, Object o2) {
+      return ((Date) o1).compareTo((Date) o2);
+    }
+}
+
+class ReverseComparator implements Comparator {
+    private final Comparator c;
+    public ReverseComparator(Comparator c) {this.c = c; }
+    public int compare(Object o1, Object o2) {
+        return c.compare(o2, o1);
+    }
+}
+
+class TurtleComparator implements Comparator {
+    public int compare(Object t1, Object t2) {
+        return ((WeightTurtle)t1).getWeight()-((WeightTurtle)t2).getWeight();
+    }
+}
--- src/com/sd/turtle/WeightTurtle.java
+++ src/com/sd/turtle/WeightTurtle.java
@@ -6,15 +6,16 @@
  */
 
 package com.sd.turtle;
-import ch.aplu.turtle.*;
 import java.awt.Color;
-public class WeightTurtle extends Turtle{
-    private int weight;
+import ch.aplu.turtle.*;
+public class WeightTurtle extends Turtle {
+    protected int weight;
     public WeightTurtle(int w) {
-      weight=w;
-      setPenColor(Color.red);
+        weight=w;
+        setColor(Color.red);
+        setPenColor(Color.red);
     }
-    public int getWeight(){
-      return weight;
+    public int getWeight() {
+        return weight;
     }
 }
